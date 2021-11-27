bc40060933bf2a793b810f94d5e3ce48e6bea82c
Anuj Rai <74399579+AnujRai612@users.noreply.github.com>
2021-11-27 13:11:10 +0530

    Nearest smallest to left

--- 1.cpp
+++ 1.cpp
@@ -7,24 +7,26 @@ vector <int> nextLargest(vector<int> arr)
   for(int i=0;i<arr.size();i++){
     if(s.size()==0)
       v.push_back(-1);
-    else if(s.size()>0 && s.top()>arr[i]){
+    else if(s.size()>0 && s.top()<arr[i]){
       v.push_back(s.top());
     }
-    else if(s.size()>0 && s.top()<=arr[i]){
-      while(s.size()>0 && s.top()<=arr[i]){
+    else if(s.size()>0 && s.top()>=arr[i]){
+      while(s.size()>0 && s.top()>=arr[i]){
         s.pop();
       }
       if(s.size()==0)
         v.push_back(-1);
-      else
-        v.push_back(s.top());
+    }
+    else{
+      v.push_back(s.top());
     }
     s.push(arr[i]);
   }
   return v;
+  
 }
 int main(){
-   vector<int> v={1,3,2,4};
+   vector<int> v={4,5,2,10,1};
    vector<int> u=nextLargest(v);
    for(int i=0;i<u.size();i++){
     cout<<u[i]<<" ";
